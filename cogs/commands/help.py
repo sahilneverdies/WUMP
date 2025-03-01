a15aa08748bf0d7b5a9b5088a38e21e6052868e6
Mr. Ghost <krishnajana295@gmail.com>
2025-03-01 14:06:02 +0530

    Olympus

--- cogs/commands/help.py
+++ cogs/commands/help.py
@@ -44,7 +44,7 @@ class HelpCommand(commands.HelpCommand):
 
     return await super().on_help_command_error(ctx, error)
 
-
+  
   async def command_not_found(self, string: str) -> None:
     ctx = self.context
     check_ignore = await ignore_check().predicate(ctx)
@@ -75,7 +75,7 @@ class HelpCommand(commands.HelpCommand):
 
     await ctx.reply(embed=embed)
 
-
+  
   async def send_bot_help(self, mapping):
     ctx = self.context
     check_ignore = await ignore_check().predicate(ctx)
@@ -88,8 +88,8 @@ class HelpCommand(commands.HelpCommand):
       await self.send_ignore_message(ctx, "command")
       return
 
-
-    embed = discord.Embed(description="⏳ **Loading Help module...**", color=color)
+    
+    embed = discord.Embed(description="<a:RedLoading:1246777916621197424> **Loading Help module...**", color=color)
     ok = await self.context.reply(embed=embed)          
     data = await getConfig(self.context.guild.id)
     prefix = data["prefix"]
@@ -99,15 +99,15 @@ class HelpCommand(commands.HelpCommand):
   cmd for cmd in self.context.bot.tree.get_commands() 
   if isinstance(cmd, app_commands.Command)
 ])
-
+    
     embed = discord.Embed(
       title="", color=0x000000)
 
-    embed.add_field(name="➡️ __**General Info:**__", value=f"🔴 Server Prefix:  **{prefix}** \n🔴 Total Commands: **{len(set(self.context.bot.walk_commands()))}**\n🔴 Total Slash Commands: **{slash}**\n🔴 **[Get Olympus](https://discord.com/oauth2/authorize?client_id=1144179659735572640&permissions=2113268958&scope=bot)** | **[Support](https://discord.com/invite/odx)**\n\n❓ __**How do you use me?**__\n>>> `{prefix}help <command/module>` to get more info regarding that command/module\nFor example: `{prefix}help antinuke`\n\n")
+    embed.add_field(name="<:olympus_ArrowRed:1253704287478091938> __**General Info:**__", value= f"<:red_dot:1222796144996777995> Server Prefix:  **{prefix}** \n<:red_dot:1222796144996777995> Total Commands: **{len(set(self.context.bot.walk_commands()))}**\n<:red_dot:1222796144996777995> Total Slash Commands: **{slash}**\n<:red_dot:1222796144996777995> **[Get Olympus](https://discord.com/oauth2/authorize?client_id=1144179659735572640&permissions=2113268958&scope=bot)** | **[Support](https://discord.com/invite/odx)**\n\n<:olympus_question:1254305536745144410> __**How do you use me?**__\n>>> `{prefix}help <command/module>` to get more info regarding that command/module\nFor example: `{prefix}help antinuke`\n\n")
 
-    embed.add_field(name="⭐ __**My Features**__", value=">>> **50+ Systems, including:**\n 🛡️ Security\n 🚨 Automoderation\n 🔧 Utility\n 🎵 Music\n 🛠️ Moderation\n 🧩 Customrole\n 🎉 Giveaway\n 🎙️ Voice\n 🎮 Games\n 👋 Welcomer\n 🪩 Autoreact & responder\n 📋 Autorole & Invc\n 🎭 Fun & AI Image Gen\n   And much more!...")
+    embed.add_field(name="<:olympus_featuers:1254254600706457612> __**My Features**__", value=">>> **50+ Systems, including:**\n <:olympus_mod:1222789854987812964> Security\n <:olympus_raidmode:1222789736465436753> Automoderation\n <:olympus_utility:1222787342474154094> Utility\n <:olympus_music:1251052387775283263> Music\n <:olympus_staff:1228227884481515613> Moderation\n <:olympus_automode:1222785253756637225> Customrole\n <:olympus_giveaways:1222784568290185246> Giveaway\n <:olympus_mic:1222790370216120382> Voice\n <:olympus_games:1222786029166264350> Games\n <:olympus_welcome:1222790448884486266> Welcomer\n <:olympus_autorespond:1222786764981272659> Autoreact & responder\n <:olympus_verification:1222786516363776062> Autorole & Invc\n <:olympus_fun:1222785541930487868> Fun & AI Image Gen\n   And much more!...")
 
-    embed.add_field(name="➡️ __**How to get help?**__", value=">>> ♨️ Use the Buttons, to swap the Pages\n♨️ Use the Menu to select all Help Pages, you want to display\n♨️ For any queries/help Contact the **[Support Team](https://discord.com/invite/odx).**")
+    embed.add_field(name="<:olympus_ArrowRed:1253704287478091938>  __**How to get help?**__", value=">>> <:dnd:1254312052302811187> Use the Buttons, to swap the Pages\n<:dnd:1254312052302811187> Use the Menu to select all Help Pages, you want to display\n<:dnd:1254312052302811187> For any queries/help Contact the **[Support Team](https://discord.com/invite/odx).**")
 
     embed.set_footer(
       text=f"Requested By {self.context.author}",
@@ -125,7 +125,7 @@ class HelpCommand(commands.HelpCommand):
 
 
 
-
+  
   async def send_command_help(self, command):
     ctx = self.context
     check_ignore = await ignore_check().predicate(ctx)
@@ -137,7 +137,7 @@ class HelpCommand(commands.HelpCommand):
     if not check_ignore:
       await self.send_ignore_message(ctx, "command")
       return
-
+    
     sonu = f">>> {command.help}" if command.help else '>>> No Help Provided...'
     embed = discord.Embed(
         description=
@@ -180,11 +180,11 @@ class HelpCommand(commands.HelpCommand):
     check_blacklist = await blacklist_check().predicate(ctx)
 
     if not check_blacklist:
-      return
+        return
 
     if not check_ignore:
-      await self.send_ignore_message(ctx, "command")
-      return
+        await self.send_ignore_message(ctx, "command")
+        return
 
     entries = [
         (
@@ -192,21 +192,20 @@ class HelpCommand(commands.HelpCommand):
             f"{cmd.short_doc if cmd.short_doc else ''}\n\u200b"
         )
         for cmd in group.commands
-      ]
+    ]
 
     count = len(group.commands)
 
+    embeds = FieldPagePaginator(
+        entries=entries,
+        title=f"{group.qualified_name.title()} [{count}]",
+        description="< > Duty | [ ] Optional\n",
+        per_page=4
+    ).get_pages()
 
-    paginator = Paginator(source=FieldPagePaginator(
-      entries=entries,
-      title=f"{group.qualified_name.title()} [{count}]",
-      description="< > Duty | [ ] Optional\n",
-      color=color,
-      per_page=4),
-                          ctx=self.context)
+    paginator = Paginator(ctx, embeds)
     await paginator.paginate()
-
-
+    
   async def send_cog_help(self, cog):
     ctx = self.context
     check_ignore = await ignore_check().predicate(ctx)
@@ -225,13 +224,13 @@ class HelpCommand(commands.HelpCommand):
       f"{cmd.short_doc if cmd.short_doc else ''}"
       f"\n\u200b",
     ) for cmd in cog.get_commands()]
-    paginator = Paginator(source=FieldPagePaginator(
+    embeds = FieldPagePaginator(
       entries=entries,
       title=f"{cog.qualified_name.title()} ({len(cog.get_commands())})",
       description="< > Duty | [ ] Optional\n\n",
       color=color,
-      per_page=4),
-                          ctx=self.context)
+      per_page=4).get_pages()
+    paginator = Paginator(ctx, embeds)
     await paginator.paginate()
 
 
@@ -255,9 +254,4 @@ class Help(Cog, name="help"):
     self.help_command = self._original_help_command
 
 
-"""
-@Author: Sonu Jana
-    + Discord: me.sonu
-    + Community: https://discord.gg/odx (Olympus Development)
-    + for any queries reach out Community or DM me.
-"""
+    
